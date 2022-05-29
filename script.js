1ede89d1633422d6e29c1bf6c236b95e3ba7d19d
UmarovIkrom <ikromme13@gmail.com>
2022-05-30 00:56:12 +0500

    restart btn functionality added

--- script.js
+++ script.js
@@ -13,15 +13,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currrentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currrentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//This function for "restart btn functionality" & start conditions branch
+const initialization = function() {
+    scores = [0, 0];
+    currrentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+initialization();
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -74,6 +86,8 @@ btnHold.addEventListener('click', function() {
             switchPlayer();
         }
     }
-});
+});  
 
+// restart btn functionality
+btnNew.addEventListener('click', initialization);
 
